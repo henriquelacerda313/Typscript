a10ef6421764add4f37a2521d4d25898fc70479c
henriquelacerda313 <161867548+henriquelacerda313@users.noreply.github.com>
2025-03-07 11:39:38 -0300

    preparando aula 03 ts

---
+++ aulas/aula03.ts
@@ -0,0 +1,13 @@
+class Curso{
+    canal= null;
+    curso=null;
+    
+    constructor(canal, curso){
+        this.canal=canal;
+        this.curso=curso;
+    }
+}
+
+let c1 = new Curso("cfbcursos", "typescript");
+console.log(c1.canal);
+console.log(c1.curso);
